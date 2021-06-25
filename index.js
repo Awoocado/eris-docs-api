8b3eb7c8e5010399c8bb43cdb4c9c25450b94087
Carlos Bullón <carlos.bullon99@gmail.com>
2021-06-25 09:36:30 -0500

    5000

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ require("express")().get('/:query?', function (req, res) {
   var query = req.params.query
   if (!query) return res.send({ content: "?" })
   res.send(docs(query.split(" ")))
-}).listen(process.env.PORT || 8000)
+}).listen(process.env.PORT || 5000)
 
